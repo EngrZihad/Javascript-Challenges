e3d177959fa62478aa8a01e6b05f58cd6564d733
Md Zihad mia <webengr.zihad@gmail.com>
2025-08-06 14:46:03 +0600

    FindBinaryNumber: Fist Solution is done

---
+++ FindBinaryNumber/script.js
@@ -0,0 +1,9 @@
+// First Solution
+
+function Binary(a,b){
+        let value=a+b;
+
+        return value.toString(2)
+}
+
+console.log(Binary(5,9))
--- sum-3-5-below-n/script.js
+++ sum-3-5-below-n/script.js
@@ -1,33 +1,40 @@
 
 // Fist Solution 
 
-function multipliesSum(n){
-    let sum=0;
-    for(let i=0;i<n;i++){
-        if(i%3==0){
-            if(i%5==0){
-                    sum+=i;
-            }else{
+// function multipliesSum(n){
+//     let sum=0;
+//     for(let i=0;i<n;i++){
+//         if(i%3==0){
+//             if(i%5==0){
+//                     sum+=i;
+//             }else{
 
-                sum+=i;
-            }
+//                 sum+=i;
+//             }
 
             
             
-        }
+//         }
 
-        else if(i%5==0){
-               sum+=i;
-            }
+//         else if(i%5==0){
+//                sum+=i;
+//             }
            
-    }
+//     }
 
 
     
 
 
-    return sum;
+//     return sum;
+// }
+
+
+// console.log(multipliesSum(10))
+
+
+// Second Solution
+
+function mysolution(n){
+
 }
-
-
-console.log(multipliesSum(10))
